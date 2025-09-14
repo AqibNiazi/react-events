7cc7b9951f37bd0a1e3ea21ad7298f469add7309
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-14 23:44:49 +0500

    React Router action and loader added in Edit Event page.

--- frontend/src/components/Events/EditEvent.jsx
+++ frontend/src/components/Events/EditEvent.jsx
@@ -1,58 +1,62 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSubmit,
+} from "react-router-dom";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { fetchEvent, updateEvent } from "../../util/util.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+// import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 import { queryClient } from "../../util/util.js";
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
+
   const { id } = useParams();
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
+    staleTime: 10000,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      const newData = data.event;
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     const newData = data.event;
 
-      await queryClient.cancelQueries({ queryKey: ["events", id] });
+  //     await queryClient.cancelQueries({ queryKey: ["events", id] });
 
-      const previousEvent = queryClient.getQueryData(["events", id]);
+  //     const previousEvent = queryClient.getQueryData(["events", id]);
 
-      queryClient.setQueriesData(["events", id], newData);
+  //     queryClient.setQueriesData(["events", id], newData);
 
-      return { previousEvent };
-    },
-    onError: (err, data, context) => {
-      queryClient.setQueryData(["events", id], context.previousEvent);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", id]);
-    },
-  });
+  //     return { previousEvent };
+  //   },
+  //   onError: (err, data, context) => {
+  //     queryClient.setQueryData(["events", id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id: id, event: formData });
-    navigate("../");
+    // mutate({ id: id, event: formData });
+    // navigate("../");
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
     navigate("../");
   }
   let content;
-
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -75,14 +79,43 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Submitting...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
   return <Modal onClose={handleClose}>{content}</Modal>;
 }
+
+//This component helps us to fetch the event data before rendering the EditEvent component
+export const loader = ({ params }) => {
+  //  queryClient.fetchQuery helps us to fetch data programmatically.
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+};
+
+// steps
+// First fetchQuery will fetch the data and store it in the cache
+// Then the useQuery in EditEvent component will get the data from the cache instead of making a network request again.
+// This will prevent the flickering of the UI when we open the edit event modal.
+
+// This action function will be executed when the form is submitted.
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(["events"]);
+  return redirect("../");
+};
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,17 @@
+import { useIsFetching } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
 export default function Header({ children }) {
+  const fetching = useIsFetching();
+  const navigate = useNavigate();
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">{fetching > 0 && <progress />}</div>
       <header id="main-header">
         <div id="header-title">
-          <h1>React Events</h1>
+          <h1 onClick={() => navigate("/events")} className="cursor-pointer">
+            React Events
+          </h1>
         </div>
         <nav>{children}</nav>
       </header>
